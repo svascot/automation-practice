1b1b97bdc698f60fcb07170561f83e0566c06a5f
svascot <svascot@gmail.com>
2022-07-11 19:34:53 -0500

    Implementation of SignIn page object

---
+++ src/main/java/pages/SignInPage.java
@@ -0,0 +1,44 @@
+package pages;
+
+import drivers.DriverSingleton;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class SignInPage {
+
+    private WebDriver driver;
+
+    public SignInPage () {
+        this.driver = DriverSingleton.getDriver();
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(id = "email")
+    private WebElement signInEmail;
+
+    @FindBy(id = "passwd")
+    private WebElement signInPassword;
+
+    @FindBy(id = "SubmitLogin")
+    private WebElement signInBtn;
+
+    @FindBy(id = "email_create")
+    private WebElement signUpEmail;
+
+    @FindBy(id = "SubmitCreate")
+    private WebElement signUpBtn;
+
+    public void logIn (String email, String password) {
+       signInEmail.sendKeys(email);
+       signInPassword.sendKeys(password);
+       signInBtn.click();
+    }
+
+    public void signUp (String email) {
+        signUpEmail.sendKeys(email);
+        signUpBtn.click();
+    }
+
+}
