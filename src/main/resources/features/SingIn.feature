17c3b9e219b8d3b668c3ac3d47f2ec5ddd910d4d
svascot <svascot@gmail.com>
2022-07-21 17:05:54 -0500

    Testing purchase two products

--- src/main/resources/features/SingIn.feature
+++ src/main/resources/features/SingIn.feature
@@ -4,3 +4,10 @@ Feature: Shopping Automation
     When I click on Sign In button
     And I specify my credentials and click Login
     Then I can log into the website
+
+  Scenario: Testing the purchase of two items
+    Given I go to the Website
+    When I add two elements to the cart
+    And I proceed to checkout
+    And I confirm address, shipping, payments and final order
+    Then the elements are bought
--- src/test/java/automation/glue/StepDefinition.java
+++ src/test/java/automation/glue/StepDefinition.java
@@ -39,10 +39,10 @@ public class StepDefinition {
         checkoutPage = new CheckoutPage();
     }
 
-    @After
+    /*@After
     public static void closeObjects() {
         DriverSingleton.closeObjectInstance();
-    }
+    }*/
 
     @Given("^I go to the Website")
     public void i_go_to_the_Website() {
@@ -55,16 +55,39 @@ public class StepDefinition {
         homePage.clickSignIn();
     }
 
+    @When("^I add two elements to the cart")
+    public void i_add_two_elements_to_the_cart () {
+        homePage.addFirstElementToCart();
+        homePage.addSecondElementToCartAndProceedToCheckout();
+    }
+
     @And("^I specify my credentials and click Login")
     public void i_specify_my_credentials_and_click_Login() {
 //        signInPage.logIn(configurationProperties.getEmail(), configurationProperties.getPassword());
         signInPage.logIn("c3Zhc2NvdEBnbWFpbC5jb20=", "MTIzNDU2Nzg=");
     }
 
+    @And("^I proceed to checkout")
+    public void i_proceed_to_checkout() {
+        checkoutPage.goToCheckout();
+    }
+
+    @And("^I confirm address, shipping, payments and final order")
+    public void i_confirm_address_shipping_payments_and_final_order() {
+        checkoutPage.confirmAddress();
+        checkoutPage.confirmShipping();
+        checkoutPage.payByBankWire();
+        checkoutPage.confirmFinalOrder();
+    }
     @Then("^I can log into the website")
     public void i_can_log_into_the_website() {
 //        assertEquals(configurationProperties.getUsername(), homePage.getUsername());
         assertEquals("Santiago Vasco", homePage.getUsername());
+    }
+
+    @Then("^the elements are bought")
+    public void the_elements_are_bought() {
+        assertEquals(Constants.COMPLETE_ORDER_MESSAGE, checkoutPage.getOrderConfirmationMessage());
     }
 
 }
